97c28ec28181c4ef445c9adb9d7995df34e71e0d
raji1123 <rajilakshmi1112223@gmail.com>
2023-07-03 21:33:36 +0530

    "Updated"

--- JavaCode/src/task8/Q10.java
+++ JavaCode/src/task8/Q10.java
@@ -3,20 +3,20 @@ package task8;
 public class Q10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// count number og digits in integer
 
 		
 		int i = 0;
-		int result;
+		
 		int sum=127;
 		while(sum!=0) {
 			sum=sum/10;
-			i=i+1;
-			result=i;
-			System.out.println(result);
+			++i;
+			
+			
 		}
 		
-		
+		System.out.println(i);
 	}
 
 }
