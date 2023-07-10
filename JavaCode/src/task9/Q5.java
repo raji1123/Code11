c7630e4e6a1df0c6d4f64c341c7c637f6fa227e0
raji1123 <rajilakshmi1112223@gmail.com>
2023-07-10 17:11:47 +0530

    "Q5 pattern"

--- JavaCode/src/task9/Q5.java
+++ JavaCode/src/task9/Q5.java
@@ -3,8 +3,64 @@ package task9;
 public class Q5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// pattern
+		
+		int n=5;
+		int i,j,k;
+		int val=n+1;
+		int space=n;
+		
+for(i=1;i<=n;i++) 
+{
+			
+			if(i<val/2)//4
+			{
+				
+				for(j=1;j<=i;j++) 
+				{
+					System.out.print(" ");
+					
+				}
+			}
+			else 
+			{
+				for(j=i;j<val;j++) 
+				{
+					System.out.print(" ");
+				}
+			}
+			
+			if(i!=val/2) 
+			{
 
-	}
-
+				System.out.print("*");
+		
+			}
+			
+			if(i<val/2) 
+			{
+				
+			space-=2;
+			for(k=1;k<=space;k++) 
+			      {
+				      System.out.print(" ");
+			      }
+			}
+			
+			if(i>val/2)
+			{
+				for(k=1;k<=space;k++) 
+				{
+					System.out.print(" ");
+				}
+				space+=2;
+			}
+			
+		        System.out.println("*");
+	   	     
+		        
 }
+		
+		
+	}
+	  }
