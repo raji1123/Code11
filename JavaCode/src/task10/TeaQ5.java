4b9c44693f22eeaf701c209d3fad492e9b4f1451
raji1123 <rajilakshmi1112223@gmail.com>
2023-07-17 13:39:55 +0530

    updated

---
+++ JavaCode/src/task10/TeaQ5.java
@@ -0,0 +1,29 @@
+package task10;
+
+public class TeaQ5 {
+
+	
+	void preparetea() {
+		System.out.println(" black tea ");
+	
+	}
+	
+	void addmilk() {
+		System.out.println("addmilk to blacktea ");
+	}
+	
+	void addsugar() {
+		
+		System.out.println("addsugar to blacktea ");
+	}
+	
+	public static void main(String[] args) {
+		TeaQ5 s=new TeaQ5();
+	     s.preparetea();
+		s.addmilk();
+        s.addsugar();
+      
+		
+	}
+
+}
